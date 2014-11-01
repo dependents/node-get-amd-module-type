4dcc68d666a7d0466efea0cff4b4656ff61234f4
Evgenus <chernyshov.eugene@gmail.com>
2014-11-01 02:48:36 +0200

    async tests

--- test/test.js
+++ test/test.js
@@ -1,34 +1,37 @@
 var getType = require('../');
 var esprima = require('esprima');
 var fs = require('fs');
+var path = require('path');
 var assert = require('assert');
 
+var expected = {
+    './factory.js': 'factory',
+    './nodep.js': 'nodeps',
+    './named.js': 'named',
+    './dep.js': 'deps',
+    './rem.js': 'rem',
+    './empty.js': null,
+    './driver.js': 'driver'
+};
+
+function testMethodAgainstExpected(method) {
+    Object.keys(expected).forEach(function(file) {
+      method(file, expected[file]);
+    });
+}
+
+function asyncTest(filename, result) {
+    it('returns `' + result + '` for ' + filename, function(done) {
+        getType(path.resolve(__dirname, filename), function (error, type) {
+            assert.equal(error, null, error);
+            assert.equal(type, result);
+            done();
+        });
+    });
+}
+
 describe('get-amd-module-type', function() {
-  it('returns the factory for the factory form', function() {
-    assert(getType(__dirname + '/factory.js') === 'factory');
-  });
-
-  it('returns nodeps for the no dependency form', function() {
-    assert(getType(__dirname + '/nodep.js') === 'nodeps');
-  });
-
-  it('returns named for the named form', function() {
-    assert(getType(__dirname + '/named.js') === 'named');
-  });
-
-  it('returns deps for the dependency form', function() {
-    assert(getType(__dirname + '/dep.js') === 'deps');
-  });
-
-  it('returns rem for the REM form (#2)', function() {
-    assert(getType(__dirname + '/rem.js') === 'rem');
-  });
-
-  it('returns null for non-amd modules', function() {
-    assert(getType(__dirname + '/empty.js') === null);
-  });
-
-  it('returns driver for a driver script', function() {
-    assert(getType(__dirname + '/driver.js') === 'driver');
+  describe('Async tests', function() {
+    testMethodAgainstExpected(asyncTest);
   });
 });
