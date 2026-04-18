d38cec3a867465bba9cab500e1d44681f642c3a6
XhmikosR <xhmikosr@gmail.com>
2026-04-18 19:29:34 +0300

    Replace mocha with uvu

--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@ const assert = require('assert').strict;
 const fs = require('fs');
 const path = require('path');
 const { promisify } = require('util');
+const { suite } = require('uvu');
 const getType = require('../index.js');
 
 const getTypeAsync = promisify(getType);
@@ -21,69 +22,73 @@ const expected = {
 
 const fixture = file => path.resolve(__dirname, 'fixtures', file);
 
-describe('get-amd-module-type', () => {
-  const cases = Object.entries(expected);
+const cases = Object.entries(expected);
 
-  describe('Async tests', () => {
-    for (const [file, result] of cases) {
-      it(`returns "${result}" for ${file}`, async() => {
-        const type = await getTypeAsync(fixture(file));
-        assert.equal(type, result);
-      });
-    }
+const asyncTests = suite('Async tests');
 
-    it('reports an error for non-existing file', async() => {
-      await assert.rejects(
-        getTypeAsync('no_such_file'),
-        { code: 'ENOENT' }
-      );
-    });
-
-    it('reports an error for file with syntax error', async() => {
-      await assert.rejects(
-        getTypeAsync(fixture('invalid.js')),
-        { name: 'SyntaxError' }
-      );
-    });
-
-    it('should throw an error if argument is missing', () => {
-      assert.throws(() => {
-        getType(fixture('dep.js'));
-      }, /^Error: callback missing$/);
-      assert.throws(() => {
-        getType();
-      }, /^Error: filename missing$/);
-    });
+for (const [file, result] of cases) {
+  asyncTests(`returns "${result}" for ${file}`, async() => {
+    const type = await getTypeAsync(fixture(file));
+    assert.equal(type, result);
   });
+}
 
-  describe('Sync tests', () => {
-    for (const [file, result] of cases) {
-      it(`returns "${result}" for ${file}`, () => {
-        const type = getType.sync(fixture(file));
-        assert.equal(type, result);
-      });
-    }
-
-    it('should throw an error if an argument is missing', () => {
-      assert.throws(() => {
-        getType.sync();
-      }, /^Error: filename missing$/);
-    });
-  });
-
-  describe('From source tests', () => {
-    for (const [file, result] of cases) {
-      it(`returns "${result}" for ${file}`, () => {
-        const source = fs.readFileSync(fixture(file), 'utf8');
-        const type = getType.fromSource(source);
-        assert.equal(type, result);
-      });
-    }
-
-    it('should throw an error if an argument is missing', () => {
-      assert.throws(() => {
-        getType.fromSource();
-      }, /^Error: source missing$/);
-    });
-  });
+asyncTests('reports an error for non-existing file', async() => {
+  await assert.rejects(
+    getTypeAsync('no_such_file'),
+    { code: 'ENOENT' }
+  );
 });
+
+asyncTests('reports an error for file with syntax error', async() => {
+  await assert.rejects(
+    getTypeAsync(fixture('invalid.js')),
+    { name: 'SyntaxError' }
+  );
+});
+
+asyncTests('should throw an error if argument is missing', () => {
+  assert.throws(() => {
+    getType(fixture('dep.js'));
+  }, /^Error: callback missing$/);
+  assert.throws(() => {
+    getType();
+  }, /^Error: filename missing$/);
+});
+
+asyncTests.run();
+
+const syncTests = suite('Sync tests');
+
+for (const [file, result] of cases) {
+  syncTests(`returns "${result}" for ${file}`, () => {
+    const type = getType.sync(fixture(file));
+    assert.equal(type, result);
+  });
+}
+
+syncTests('should throw an error if an argument is missing', () => {
+  assert.throws(() => {
+    getType.sync();
+  }, /^Error: filename missing$/);
+});
+
+syncTests.run();
+
+const fromSourceTests = suite('From source tests');
+
+for (const [file, result] of cases) {
+  fromSourceTests(`returns "${result}" for ${file}`, () => {
+    const source = fs.readFileSync(fixture(file), 'utf8');
+    const type = getType.fromSource(source);
+    assert.equal(type, result);
+  });
+}
+
+fromSourceTests('should throw an error if an argument is missing', () => {
+  assert.throws(() => {
+    getType.fromSource();
+  }, /^Error: source missing$/);
+});
+
+fromSourceTests.run();
